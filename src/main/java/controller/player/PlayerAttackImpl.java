b46493e834f819d8b13b729428f9e230ebd36d9b
Alice Conti <alice.conti7@studio.unibo.it>
2022-06-09 16:50:11 +0200

    deleted the counterattack in attackHero

--- src/main/java/controller/player/PlayerAttackImpl.java
+++ src/main/java/controller/player/PlayerAttackImpl.java
@@ -55,7 +55,7 @@ public class PlayerAttackImpl implements PlayerAttack {
 	
 	public void getHit(int enemyID,int enemyResponseHit) {
 		gg.player.getLife().setLifePoints(gg.player.getLife().getLifePoints()-enemyResponseHit);
-		counter(enemyID, player.getAttackPoints());
+		//counter(enemyID, player.getAttackPoints());
 		if(player.getLife().getLifePoints()<=0) {   //se il player non ha più vita,il gplayer muore e il turno è finito
 			System.out.println("L'eroe è morto!");
 			System.exit(0);
